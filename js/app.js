b22007c2a50d21d526a20e050f2d22aa0bb39800
Melissa Garcia <megarciade@outlook.com>
2022-03-16 17:26:29 -0400

    Fix body when mobile nav is open

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
 function toggleMobileMenu(menu) {
     menu.classList.toggle('open');
 }
+
+if ($('.mobile-menu').is(':visible')) {
+    $('body').addClass("fixed-position");
+ } else {
+    $('body').removeClass("fixed-position");
+ }
+
