fa0ca6b4b067b6ce862450867cabc1bd564240e0
Melissa Garcia <megarciade@outlook.com>
2022-04-03 12:56:38 -0400

    Mobile Menu fix
    fixed issue with menu scroll on mobile

--- js/app.js
+++ js/app.js
@@ -1,10 +1,7 @@
+
 function toggleMobileMenu(menu) {
     menu.classList.toggle('open');
+    document.body.classList.toggle('lock-scroll');
 }
 
-if ($('.mobile-menu').is(':visible')) {
-    $('body').addClass("fixed-position");
- } else {
-    $('body').removeClass("fixed-position");
- }
 
